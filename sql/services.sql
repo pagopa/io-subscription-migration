1a2daac8a04ba7515c79ce444a6d690a66ad26fe
LoFrance <lorenzo.franceschini@pagopa.it>
2022-01-31 20:44:41 +0100

    Remove unecessary sql

--- sql/services.sql
+++ sql/services.sql
@@ -14,23 +14,3 @@ CREATE TABLE IF NOT EXISTS "ServicesMigration"."Services"
     "serviceName" character varying  NOT NULL,
     "updateAt" timestamp default current_timestamp,
 );
-
-
-INSERT INTO "ServicesMigration"."Services"(
-	"subscriptionId", "organizationFiscalCode", "sourceId", "sourceName", "sourceSurname", "sourceEmail", "serviceVersion", "serviceName")
-	VALUES (1, 2, 3, 'Lorenzo', 'Franceschini', 'postaforum@gmail.com', 0, 'Test Service');
-
-
-INSERT INTO "ServicesMigration"."Services"(
-	"subscriptionId", "organizationFiscalCode", "sourceId", "sourceName",
-	"sourceSurname", "sourceEmail", "serviceVersion", "serviceName")
-	VALUES (1, 33, 3, 'Lorenzo', 'Franceschini', 'postaforum@gmail.com', 1, 'Test Service')
-	ON CONFLICT ("subscriptionId")
-
-	DO UPDATE
-		SET "organizationFiscalCode" = "excluded"."organizationFiscalCode",
-		"serviceVersion" = "excluded"."serviceVersion",
-		"serviceName" = "excluded"."serviceName"
-		WHERE "ServicesMigration"."Services"."status" <> 'PENDING'
-        AND "ServicesMigration"."Services"."serviceVersion" < "excluded"."serviceVersion"
-
