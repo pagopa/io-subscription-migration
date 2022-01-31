068aab17b268b1ec940337b2041cb6529cb9d385
LoFrance <lorenzo.franceschini@pagopa.it>
2022-01-31 21:04:00 +0100

    Refactor function parameters

--- utils/dbconnector.ts
+++ utils/dbconnector.ts
@@ -3,29 +3,37 @@ import { Pool } from "pg";
 import { IDecodableConfigPostgreSQL } from "./config";
 
 const pool = (
-  dbUser: NonEmptyString,
-  dbPassword: NonEmptyString,
-  dbHost: NonEmptyString,
-  dbPort: NonEmptyString,
-  dbDatabaseName: NonEmptyString,
-  dbIdleTimeout: number = 30000
-  // eslint-disable-next-line max-params
+  db: {
+    readonly name: NonEmptyString;
+    readonly host: NonEmptyString;
+    readonly port: NonEmptyString;
+  },
+  credentials: {
+    readonly user: NonEmptyString;
+    readonly password: NonEmptyString;
+  },
+  options: { readonly idleTimeout: number } = { idleTimeout: 30000 }
 ): Pool =>
   new Pool({
-    connectionString: `postgres://${dbUser}:${dbPassword}@${dbHost}:${dbPort}/${dbDatabaseName}`,
-    idleTimeoutMillis: dbIdleTimeout,
+    connectionString: `postgres://${credentials.user}:${credentials.password}@${db.host}:${db.port}/${db.name}`,
+    idleTimeoutMillis: options.idleTimeout,
     max: 20
   });
 
-// export default pool;
-
 export const clientDB = (config: IDecodableConfigPostgreSQL): Pool =>
   pool(
-    config.DB_USER,
-    config.DB_PASSWORD,
-    config.DB_HOST,
-    config.DB_PORT,
-    config.DB_NAME
+    {
+      host: config.DB_HOST,
+      name: config.DB_NAME,
+      port: config.DB_PORT
+    },
+    {
+      password: config.DB_PASSWORD,
+      user: config.DB_USER
+    },
+    {
+      idleTimeout: config.DB_IDLE_TIMEOUT
+    }
   );
 
 export default clientDB;
