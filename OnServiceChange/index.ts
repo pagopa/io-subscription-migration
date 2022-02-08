7d9b571366bfa5f64aaeb8185cf226ab61403c9a
Emanuele De Cupis <ema.decu@gmail.com>
2022-02-08 12:41:21 +0100

    [#ic-252] remove previous implementation and dispatch jobs

--- OnServiceChange/index.ts
+++ OnServiceChange/index.ts
@@ -1,6 +1,4 @@
 import { getConfigOrThrow } from "../utils/config";
-import clientDB from "../utils/dbconnector";
-import { getApiClient } from "../utils/apim";
 import { initTelemetryClient } from "../utils/appinsight";
 import { createServiceChangeHandler } from "./handler";
 
@@ -9,22 +7,6 @@ const config = getConfigOrThrow();
 // Setup Appinsight
 const telemetryClient = initTelemetryClient(config);
 
-// Setup PostgreSQL DB Pool
-const client = clientDB(config);
-const apimClient = getApiClient(
-  {
-    clientId: config.APIM_CLIENT_ID,
-    secret: config.APIM_SECRET,
-    tenantId: config.APIM_TENANT_ID
-  },
-  config.APIM_SUBSCRIPTION_ID
-);
-
-const handleServicesChange = createServiceChangeHandler(
-  config,
-  apimClient,
-  client,
-  telemetryClient
-);
+const handleServicesChange = createServiceChangeHandler(telemetryClient);
 
 export default handleServicesChange;
