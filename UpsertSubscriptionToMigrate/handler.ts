bcfe6092d1358323b9249336526b1ffa62aaa3f5
Emanuele De Cupis <ema.decu@gmail.com>
2022-02-08 11:51:07 +0100

    [#ic-252] [#ic-252] [#ic-252] add json input middleware

--- UpsertSubscriptionToMigrate/handler.ts
+++ UpsertSubscriptionToMigrate/handler.ts
@@ -1,15 +1,17 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { ApiManagementClient } from "@azure/arm-apimanagement";
-import { Context } from "io-ts";
 import { Pool } from "pg";
 import { initTelemetryClient } from "../utils/appinsight";
 import { IConfig } from "../utils/config";
+import { withJsonInput } from "../utils/misc";
 
 export const createHandler = (
-  config: IConfig,
-  apimClient: ApiManagementClient,
-  client: Pool,
-  telemetryClient: ReturnType<typeof initTelemetryClient>
-) => (context: Context, documents: unknown): Promise<void> => {
-  throw new Error("not implemented yet!");
-};
+  _config: IConfig,
+  _apimClient: ApiManagementClient,
+  _client: Pool,
+  _telemetryClient: ReturnType<typeof initTelemetryClient>
+): Parameters<typeof withJsonInput>[0] =>
+  withJsonInput(
+    (_context, _item): Promise<void> => {
+      throw new Error("not implemented yet!");
+    }
+  );
