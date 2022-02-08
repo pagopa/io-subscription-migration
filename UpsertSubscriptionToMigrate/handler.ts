faa5cefe2910e5a86df953667f3836c87b419b9a
Emanuele De Cupis <ema.decu@gmail.com>
2022-02-08 12:10:08 +0100

    [#ic-252] validate incoming message

--- UpsertSubscriptionToMigrate/handler.ts
+++ UpsertSubscriptionToMigrate/handler.ts
@@ -1,8 +1,14 @@
 import { ApiManagementClient } from "@azure/arm-apimanagement";
+import { pipe } from "fp-ts/lib/function";
 import { Pool } from "pg";
+import { readableReport } from "@pagopa/ts-commons/lib/reporters";
+import * as E from "fp-ts/lib/Either";
+import * as TE from "fp-ts/lib/TaskEither";
 import { initTelemetryClient } from "../utils/appinsight";
 import { IConfig } from "../utils/config";
 import { withJsonInput } from "../utils/misc";
+
+import { IncomingQueueItem } from "./types";
 
 export const createHandler = (
   _config: IConfig,
@@ -11,7 +17,21 @@ export const createHandler = (
   _telemetryClient: ReturnType<typeof initTelemetryClient>
 ): Parameters<typeof withJsonInput>[0] =>
   withJsonInput(
-    (_context, _item): Promise<void> => {
-      throw new Error("not implemented yet!");
+    (context, item): Promise<void> => {
+      const logPrefix = context.executionContext.functionName;
+      return pipe(
+        item,
+        IncomingQueueItem.decode,
+        E.mapLeft(failures => {
+          const err = new Error(readableReport(failures));
+          context.log(`${logPrefix}|Invalid incoming message: ${err.message}`);
+          return err;
+        }),
+        TE.fromEither,
+        TE.map(_ => void 0 /* we wxpect no return */),
+        TE.getOrElse(err => {
+          throw err;
+        })
+      )();
     }
   );
