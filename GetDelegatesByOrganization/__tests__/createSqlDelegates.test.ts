a373f75a611a30db713d7eea51fe665a8581818f
LoFrance <lorenzo.franceschini@pagopa.it>
2022-02-15 10:04:44 +0100

    IC-268-Add knex query and unit test

---
+++ GetDelegatesByOrganization/__tests__/createSqlDelegates.test.ts
@@ -0,0 +1,26 @@
+import {
+  NonEmptyString,
+  OrganizationFiscalCode
+} from "@pagopa/ts-commons/lib/strings";
+import { createSqlDelegates } from "../handler";
+
+const mockDBConfig = {
+  DB_HOST: "localhost" as NonEmptyString,
+  DB_IDLE_TIMEOUT: 3000,
+  DB_NAME: "test" as NonEmptyString,
+  DB_PASSWORD: "psw" as NonEmptyString,
+  DB_PORT: 5454,
+  DB_SCHEMA: "Schema" as NonEmptyString,
+  DB_TABLE: "Table" as NonEmptyString,
+  DB_USER: "User" as NonEmptyString
+};
+
+describe("Create SQL for Delegates", () => {
+  it("should generate a valid SQL", () => {
+    const expectedSql = `select "sourceId", "sourceName", "sourceSurname", "sourceEmail", count("subscriptionId") from "Schema"."Table" where "organizationFiscalCode" = '12345678901' group by "sourceId", "sourceName", "sourceSurname", "sourceEmail"`;
+    const sql = createSqlDelegates(mockDBConfig)(
+      "12345678901" as OrganizationFiscalCode
+    );
+    expect(sql).toBe(expectedSql);
+  });
+});
--- GetDelegatesByOrganization/handler.ts
+++ GetDelegatesByOrganization/handler.ts
@@ -8,13 +8,34 @@ import { wrapRequestHandler } from "@pagopa/ts-commons/lib/request_middleware";
 import * as express from "express";
 import { pipe } from "fp-ts/lib/function";
 import * as TE from "fp-ts/lib/TaskEither";
+import {
+  NonEmptyString,
+  OrganizationFiscalCode
+} from "@pagopa/ts-commons/lib/strings";
+import knex from "knex";
 import { OrganizationDelegates } from "../generated/definitions/OrganizationDelegates";
+import { IDecodableConfigPostgreSQL } from "../utils/config";
 
 type GetDelegatesByOrganizationResponseHandler = () => Promise<
   | IResponseSuccessJson<{ readonly data: OrganizationDelegates }>
   | IResponseErrorInternal
   | IResponseErrorNotFound
 >;
+
+export const createSqlDelegates = (dbConfig: IDecodableConfigPostgreSQL) => (
+  organizationFiscalCode: OrganizationFiscalCode
+): NonEmptyString =>
+  knex({
+    client: "pg"
+  })
+    .withSchema(dbConfig.DB_SCHEMA)
+    .table(dbConfig.DB_TABLE)
+    .select(["sourceId", "sourceName", "sourceSurname", "sourceEmail"])
+    .count("subscriptionId")
+    .from(dbConfig.DB_TABLE)
+    .where({ organizationFiscalCode })
+    .groupBy(["sourceId", "sourceName", "sourceSurname", "sourceEmail"])
+    .toQuery() as NonEmptyString;
 
 // TO DO: This is the Handler and it's to be implemented!
 const createHandler = (): GetDelegatesByOrganizationResponseHandler => (): ReturnType<
