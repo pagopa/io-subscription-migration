ccff51fd75df6b0987202bf972c71f183bce403f
Lorenzo Franceschini <78347349+LoFrance@users.noreply.github.com>
2022-02-15 15:30:13 +0100

    [#IC-266] List delegates OpenAPI specific (#26)
    * Add open api specifics
    
    * Refactor removed test endpoint
    
    * Refactor: moved queryDataTable inside dbconnector
    
    * Refactor: rename dbconnector in db
    
    * Add query to DB for status
    
    * [IC-254] Refactor error for ResponseErrorInternal on createHandler
    
    * IC-254-Refactor-Errors
    
    * IC-254-Refactor resultset and add test
    
    * IC-254-Fix a typo on function comment
    
    * IC-254-Refactor SQL with Knex
    
    * IC-254-Refactor knex query with unit test and fix some tests
    
    * IC-254-Fix OpenAPI
    
    * IC-266-Add operation on OpenAPI
    
    * IC-266-Add delegates to route path
    
    Co-authored-by: lordkenzo <lordkenzo@outlook.com>

---
+++ openapi/__tests__/index.test.ts
@@ -0,0 +1,19 @@
+import { Email } from "../../generated/definitions/Email";
+import { OrganizationDelegates } from "../../generated/definitions/OrganizationDelegates";
+import * as E from "fp-ts/lib/Either";
+describe("Array Delegates", () => {
+  it("should validate a valid Delegates response", () => {
+    const delegates: OrganizationDelegates = [
+      {
+        sourceId: "123",
+        firstName: "TestName",
+        lastName: "TestSurname",
+        email: "test@email.com" as Email
+      }
+    ];
+
+    const res = OrganizationDelegates.decode(delegates);
+
+    expect(E.isRight(res)).toBe(true);
+  });
+});
