27caae5710efc38a6016d31ea8966176ebf2eded
Lorenzo Franceschini <78347349+LoFrance@users.noreply.github.com>
2022-10-10 15:38:02 +0200

    IC-416 Add handler and query to retrieve data (#35)

--- GetLatestMigrationsStatus/__tests__/handler.test.ts
+++ GetLatestMigrationsStatus/__tests__/handler.test.ts
@@ -1,8 +1,11 @@
+import { Context } from "@azure/functions";
 import {
   NonEmptyString,
   OrganizationFiscalCode
 } from "@pagopa/ts-commons/lib/strings";
+import { Pool, QueryResult } from "pg";
 import { SubscriptionStatus } from "../../GetOwnershipClaimStatus/handler";
+import { IConfig } from "../../utils/config";
 import { createHandler, createSqlStatus } from "../handler";
 
 const mockDBConfig = {
@@ -15,12 +18,26 @@ const mockDBConfig = {
   DB_TABLE: "Table" as NonEmptyString,
   DB_USER: "User" as NonEmptyString
 };
-
+const mockConfig = ({} as unknown) as IConfig;
+const mockQueryResult = {
+  command: "SELECT",
+  rowCount: 0
+} as QueryResult;
+const mockPool = {
+  query: jest.fn().mockImplementation(() => Promise.resolve(mockQueryResult))
+};
+const mockContext = {
+  log: jest.fn(),
+  executionContext: { functionName: jest.fn() }
+};
 describe("Create Handler Test", () => {
   it("should return a Reponse Error Internal", async () => {
-    const handler = createHandler();
+    const handler = createHandler(mockConfig, (mockPool as unknown) as Pool);
 
-    const response = await handler();
+    const response = await handler(
+      (mockContext as unknown) as Context,
+      "12345678901" as OrganizationFiscalCode
+    );
 
     expect(response.kind).toBe("IResponseErrorInternal");
   });
@@ -28,10 +45,9 @@ describe("Create Handler Test", () => {
 
 describe("Create SQL Query", () => {
   it("should generate a valid SQL Query", () => {
-    const expectedSql = `select distinct "t"."sourceEmail", "t"."status" from "TableSchema"."Table" as "t" inner join (select "sourceEmail", max("updateAt") as "latestOp" from "TableSchema"."Table" where "organizationFiscalCode" = '12345678901' and not "status" = 'INITIAL' group by "sourceEmail") as "x" on "x"."sourceEmail" = "t"."sourceEmail" and "latestOp" = "t"."updateAt"`;
+    const expectedSql = `select "sourceId", "sourceName", "sourceSurname", "sourceEmail", sum(CASE WHEN "m"."status" = 'INITIAL' THEN 1 ELSE 0 END) as initial, sum(CASE WHEN "m"."status" = 'PROCESSING' THEN 1 ELSE 0 END) as processing, sum(CASE WHEN "m"."status" = 'FAILED' THEN 1 ELSE 0 END) as failed, sum(CASE WHEN "m"."status" = 'COMPLETED' THEN 1 ELSE 0 END) as completed from "TableSchema"."Table" as "m" where "organizationFiscalCode" = '12345678901' group by "sourceId", "sourceName", "sourceSurname", "sourceEmail"`;
     const sql = createSqlStatus(mockDBConfig)(
-      "12345678901" as OrganizationFiscalCode,
-      SubscriptionStatus.INITAL
+      "12345678901" as OrganizationFiscalCode
     );
 
     expect(sql).toBe(expectedSql);
