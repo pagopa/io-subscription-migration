887a792c00af651bceb9b8e02319cd516d7c897c
Emanuele De Cupis <ema.decu@gmail.com>
2022-11-07 09:51:34 +0100

    [#IOPSC-105] DB Schema (#42)

---
+++ schema/migrations/db/V2__migrations_table.sql
@@ -0,0 +1,24 @@
+CREATE TABLE IF NOT EXISTS "${schemaName}".migrations
+(
+    "subscriptionId" character(50)  NOT NULL,
+    "organizationFiscalCode" character(11)  NOT NULL,
+    "serviceVersion" integer NOT NULL,
+    "serviceName" text,
+
+    "sourceId" character(26)  NOT NULL,
+    "sourceName" text ,
+    "sourceSurname" text ,
+    "sourceEmail" text ,
+    
+    -- In order to avoid null fileds, we set FALSE as default
+    -- This is harmless as
+    --  * we will ensure this field to be set at insert time
+    --  * the field is not considered yet by the application (at the time of writing)
+    "isVisible" boolean NOT NULL default false,
+    "hasBeenVisibleOnce"  boolean NOT NULL default false,
+
+    status text  NOT NULL DEFAULT 'INITIAL'::bpchar,
+    note text ,
+    "updateAt" timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT migrations_pkey PRIMARY KEY ("subscriptionId")
+)
