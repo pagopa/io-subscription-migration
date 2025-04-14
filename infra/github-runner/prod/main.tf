e8b8e9aed778901ff21430d74adafd918ffdadd5
Krusty93 <andrea.grillo@pagopa.it>
2025-04-14 10:59:53 +0200

    upgrade module

--- infra/github-runner/prod/main.tf
+++ infra/github-runner/prod/main.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "<= 3.100.0"
+      version = "~> 4.0"
     }
   }
 
   backend "azurerm" {
     resource_group_name  = "terraform-state-rg"
-    storage_account_name = "tfappprodio"
+    storage_account_name = "iopitntfst001"
     container_name       = "terraform-state"
-    key                  = "io-subscription-migration.github-runner.tfstate"
+    key                  = "io-subscription-migration.github-runner.prod.tfstate"
   }
 }
 
